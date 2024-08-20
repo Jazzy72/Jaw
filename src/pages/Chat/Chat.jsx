45245ee657f892744b8b1446d9a7498a3e1566bd
Deepak Kumar <diwakardeepak072@gmail.com>
2024-08-20 16:31:08 +0530

    Update Chat.jsx

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -4,9 +4,6 @@ import LeftSidebar from '../../components/LeftSidebar/LeftSidebar';
 import ChatBox from '../../components/ChatBox/ChatBox';
 import RightSidebar from '../../components/RightSidebar/RightSidebar';
 import { AppContext } from '../../context/AppContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import assets from '../../assets/assets';
 
 const Chat = () => {
 
@@ -22,10 +19,9 @@ const Chat = () => {
   return (
     <div className='chat'>
       {loading ? (
-        // <p className='loading'>Loading...</p>
-        <p className='loading'>
-          <FontAwesomeIcon icon={faSpinner} />
-        </p>
+        <div className="loader">
+          <div className="spinner"></div>
+        </div>
       ) : (
         <div className="chat-container">
           <LeftSidebar />
